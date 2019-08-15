f2aca41db619b7ebdcc7cc0b255d9ba347eb0829
odso Amith <54076710+OdsoAmith@users.noreply.github.com>
2019-08-15 14:03:54 +0530

    Update Day5.c

--- Day5.c
+++ Day5.c
@@ -1,3 +1,4 @@
+/*Given an integer, n , print its first 10 multiples. Each multiple n x i (where 1<=i<10) should be printed on a new line in the form: n x i = result.*/
 #include <assert.h>
 #include <limits.h>
 #include <math.h>
